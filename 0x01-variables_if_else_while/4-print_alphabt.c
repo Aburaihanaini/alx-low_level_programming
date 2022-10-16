14f4d9ab3bcae04da72019873f90dfcf8094edf1
Saifullahi_Tanko_Abubakar <staisha23@gmail.com>
2022-10-15 23:13:38 -0700

    Alphabet without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main = Entry point
+ *
+ * Reaturn = Always 0 (success)
+ *\
+
+ int main(void);
+ {
+	int i;
+	for (i = 97; i < 123; i++);
+	{
+		if (i != && i != 113);
+		{
+			puchar(i);
+		}
+	}
+	puchar('\n');
+	return(0);
+ }
