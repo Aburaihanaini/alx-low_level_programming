bf8a65e82b8b2a6695c20dc517715492295fe6d1
Saifullahi_Tanko_Abubakar <staisha23@gmail.com>
2022-10-17 08:35:50 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,11 +5,11 @@
  * Return: Alway 0
  *
  */
-int main(void);
+int main(void)
 {
 	char a;
 
-	for (a = 'z'; a >= 'a' ; a--);
+	for (a = 'z'; a >= 'a' ; a--)
 		putchar(a);
 	putchar('\n');
 	return (0);
